260b142587443a0e746ba85035ce124822bc5ab5
Anjali <anjali.kaimal@gmail.com>
2024-01-27 19:49:25 +0530

    install react and tailwind and add routes and pages

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignUp from "./pages/SignUp";
+
+function App() {
+  return (
+    <BrowserRouter className="App">
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/sign-in" element={<SignIn/>}/>
+        <Route path="/sign-up" element={<SignUp/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
