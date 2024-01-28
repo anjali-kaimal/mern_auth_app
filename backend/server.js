51e82959bfb655dd0ef8cbd0cff2504d54aa8c0f
Anjali <anjali.kaimal@gmail.com>
2024-01-28 10:33:41 +0530

    create database connection to mongo

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,21 @@
-import express  from "express";
+import express from "express";
+import mongoose, { mongo } from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
+// connect to mongodb and check if connection is success
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("MongoDB connection successful!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+  
 // create application
-const app=express();
+const app = express();
 
-app.listen(5000,()=>{
-    console.log("Server listening on port 5000!")
+app.listen(5000, () => {
+  console.log("Server listening on port 5000!");
 });
