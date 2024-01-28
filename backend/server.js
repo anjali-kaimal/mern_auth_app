550a079580d3efce43fda7d2cad51d0be9249f0d
Anjali <anjali.kaimal@gmail.com>
2024-01-28 15:39:48 +0530

    add a middleware and a function to handle errors

--- backend/server.js
+++ backend/server.js
@@ -26,4 +26,13 @@ app.listen(5000, () => {
 app.use("/api/user",userRoutes);
 app.use("/api/auth",authRoutes);
 
-
+// middleware
+app.use((err,req,res,next)=>{
+    const statusCode=err.statusCode||500;
+    const message=err.message||"Internal Server Error";
+    return res.status(statusCode).json({
+        success:false,
+        message,
+        statusCode,
+    });
+})
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,7 +6,7 @@ function Header() {
     <div className='bg-secondary text-primary'>
         <div className='flex flex-row justify-between p-5 sm:px-2 sm:py-4 sm:flex-col sm:gap-4'>
             <Link to='/'>
-            <h1 className='font-bold sm:flex sm:justify-center text-fourth text-2xl'>Auth App</h1>
+            <h1 className='font-bold sm:flex sm:justify-center text-fourth text-2xl gap-3'><i class="ri-shield-user-fill text-2xl"></i>Auth App</h1>
             </Link>
             <ul className='flex flex-row gap-16 mr-10 sm:gap-0 sm:mr-2 sm:justify-between'>
                 <li><Link to='/'>Home</Link></li>
